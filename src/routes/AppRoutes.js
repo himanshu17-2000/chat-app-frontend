de4b225b4ec16b3cdee975fd7366987b0ef03518
himanshu17-2000 <earthboxer@gmail.com>
2023-09-15 12:19:43 +0530

    route commented for demmo

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -11,7 +11,7 @@ export default function AppRoutes() {
       <BrowserRouter>
         <Routes>
           {/* <Route exact path="" element={<Chat />} /> */}
-          <Route exact path="authenticate" element={<Authentication />} />
+          <Route exact path="" element={<Authentication />} />
         </Routes>
       </BrowserRouter> 
     </div>
